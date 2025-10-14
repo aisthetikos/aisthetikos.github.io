f9154b510cac5ea5ce3cdaa4e6853fdbcda94711
aisthetikos <gbeck4182@gmail.com>
2025-10-13 23:22:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-class FarinelliBreathingExercise {
-    constructor() {
-        this.currentPhase = 'ready';
-        this.currentCount = 4;
-        this.startingCount = 4;
-        this.maxCount = 10;
-        this.currentCycle = 1;
-        this.totalCycles = 0;
-        this.timeRemaining = 0;
-        this.interval = null;
-        this.isRunning = false;
-        this.isPaused = false;
-        
-        // Audio context for chimes and metronome
-        this.audioContext = null;
-        this.chimeFrequencies = {
+        if (this.isRunning) {
+            this.currentCycle++;
+            this.currentCount++;
+            this.currentCountElement.textContent = this.currentCount;
+            this.updateProgress();
+            
+            if (this.currentCount <= this.maxCount) {
+                // Seamless transition to next cycle
+                await this.runCycle();
+            } else {
+                // Exercise complete
+                this.completeExercise();
+            }
+        }equencies = {
             inhale: 523.25, // C5
             hold: 659.25,   // E5
             exhale: 392.00  // G4
@@ -149,8 +147,7 @@ class FarinelliBreathingExercise {
             this.updateProgress();
             
             if (this.currentCount <= this.maxCount) {
-                // Small pause between cycles
-                await this.sleep(500);
+                // Seamless transition to next cycle
                 await this.runCycle();
             } else {
                 // Exercise complete

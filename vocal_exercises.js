71208065a05c6ac8ffa4a3c88448f049b8a25118
aisthetikos <gbeck4182@gmail.com>
2025-10-13 23:35:10 -0700

    Create vocal_exercises.js

---
+++ vocal_exercises.js
@@ -0,0 +1,14 @@
+const VOCAL_EXERCISES = [
+  {
+    "id": "exercise_123",
+    "title": "Lamperti Exercise 1",
+    "difficulty": "beginner",
+    "audio": {
+      "tempo": 120,
+      "notes": [
+        {"note": "C4", "duration": 1, "type": "quarter"},
+        // ... more notes
+      ]
+    }
+  }
+];
